f007112adfa48b29e5b675c5db613cdc9009dd69
Mark <menglish99@gmail.com>
2021-04-15 01:13:28 +0930

    inlined

--- calc/src/index.js
+++ calc/src/index.js
@@ -1,12 +1,31 @@
 import "bootstrap";
-import $ from "jquery";
 import "alpinejs";
 
-// JQuery
-$(document).ready(() => {
-  var inputLst = ["followersFB", "followersInsta", "followersLI", "associated", "associatedLeader", "associatedFollowers", "perceivedQuality", "customerContact", "potential", "loyalty"];
+window.form = function () {
+  return {
+    followersFB: 1000,
+    followersInsta: 1000,
+    followersLI: 1000,
+    associated: null,
+    associatedLeader: null,
+    associatedFollowers: 200,
+    perceivedQuality: 0,
+    customerContact: 0,
+    potential: 0,
+    loyalty: 0,
+    scoreRaw: 0,
 
-  inputLst.forEach((fn) => {
-    console.log(fn);
-  });
-});
+    doCalc() {
+      this.scoreRaw =
+        parseInt(this.followersFB) +
+        parseInt(this.followersInsta) +
+        parseInt(this.followersLI) +
+        parseInt(this.associatedFollowers) +
+        parseInt(this.perceivedQuality) +
+        parseInt(this.customerContact) +
+        parseInt(this.potential) +
+        parseInt(this.loyalty);
+      return this.scoreRaw;
+    },
+  };
+};
